5a339b31490a85bf8c23603fe9951fee68dd66af
Majixiao <mjxLearn@yeah.com>
2022-08-18 11:56:11 +0800

    first

---
+++ src/main/java/com/mjx/MyJavaSpringBootVueApplication.java
@@ -0,0 +1,13 @@
+package com.mjx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaSpringBootVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJavaSpringBootVueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mjx/MyJavaSpringBootVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaSpringBootVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
